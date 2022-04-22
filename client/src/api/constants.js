4451cf726a355056fe287119439ce4f685f71105
just-do-halee <just.do.halee@gmail.com>
2022-04-23 02:56:44 +0900

    Profile 컴포넌트, Board 컴포넌트 생성 및 수정. 전체적으로 기초 css(display, width, margin...) 세팅 완료

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://hacker-punk.herokuapp.com/'; // "https://jsonplaceholder.typicode.com/";
+export const BASE_URL = 'https://hacker-punk.herokuapp.com/';
 
 export const url = (...args) => {
   return BASE_URL + args.join('/');
---
+++ client/src/components/Profile.js
@@ -0,0 +1,62 @@
+import { React, styled, Div } from '../common';
+
+import LogoSrc from '../assets/hplogo.gif';
+
+const Container = styled(Div)`
+  z-index: 999;
+  position: absolute;
+  right: 10vw;
+  top: 12vh;
+  width: 11rem;
+  height: 6rem;
+  border: solid 1px whitesmoke;
+  padding: 10px;
+  flex-direction: column;
+  font-family: 'Gill Sans', sans-serif;
+`;
+
+const InnerContainer = styled(Div)`
+  justify-content: space-between;
+  align-items: center;
+  word-break: break-all;
+`;
+
+const Image = styled.img`
+  width: 25%;
+  height: 25%;
+  border-radius: 50%;
+  margin-right: 4px;
+`;
+
+const Span = styled.label`
+  font-size: 0.8rem;
+  font-weight: 600;
+  margin-right: 10px;
+`;
+
+const Address = styled.p`
+  font-size: 0.77rem;
+`;
+
+const Token = styled.p`
+  font-size: 0.8rem;
+  color: white;
+`;
+
+function Profile() {
+  return (
+    <Container>
+      <InnerContainer>
+        <Image src={LogoSrc} />
+        <Address>3h5gEDDxMAPfvaYi5XLuf6Hn6HRANAu1</Address>
+      </InnerContainer>
+      <InnerContainer>
+        <Span>Lv.99</Span>
+        <Token>20521003</Token>
+        <Span>hp</Span>
+      </InnerContainer>
+    </Container>
+  );
+}
+
+export default Profile;
