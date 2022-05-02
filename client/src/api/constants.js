59084be423f48eb12029f7c1e811c4ff92165dba
just-do-halee <just.do.halee@gmail.com>
2022-05-02 13:36:13 +0900

    contract 주소 또 수정

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -8,7 +8,7 @@ export const url = (...args) => {
   return BASE_URL + args.join("/");
 };
 
-export const CONTRACT_ADDR = "0x0f3004F96583A83CE89c3774e5A280e5c6DB6986";
+export const CONTRACT_ADDR = "0x72c65a200Ef2Acb48C511e66a0CF31b380DA7dB4";
 
 export const HP_CONTRACT_ADDR = "0x4f669Db16F03Ae324a83EC5BcE73aFeB28cf3878";
 
