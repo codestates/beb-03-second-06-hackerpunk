b88aa412f114246b1d79171e2601216d12b65a49
just-do-halee <just.do.halee@gmail.com>
2022-05-02 09:12:25 +0900

    ExternalHP 컨트랙트 주소 수정

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -8,7 +8,7 @@ export const url = (...args) => {
   return BASE_URL + args.join("/");
 };
 
-export const CONTRACT_ADDR = "0x15341C93220040198102Ff63D7041041A473ad10";
+export const CONTRACT_ADDR = "0x133cfaBd0Bc2F4fB6727FbcE29fEAf545564786f";
 
 export const HP_CONTRACT_ADDR = "0xc294b0F91ecd455bd383d6784c63129C151E4E15";
 
